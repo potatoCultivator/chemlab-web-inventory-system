6d9720f1d4bca1f2e017fd5e60c85ad3298cc08a
caloy <21-1-02391@vsu.edu.ph>
2024-09-07 21:49:49 +0800

    add double confirmation for deleting an Item

--- src/pages/tools-equipments/AddItemDialog.jsx
+++ src/pages/tools-equipments/AddItemDialog.jsx
@@ -6,17 +6,20 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Grid from '@mui/material/Grid';
+import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress
 
 // firebase import
-import uploadTE from '../TE_Backend'; 
+import uploadTE from '../TE_Backend';
 
 export default function AddItemDialog({ onDone }) {
   const [open, setOpen] = React.useState(false);
+  const [loading, setLoading] = React.useState(false); // Add loading state
   const [item, setItem] = React.useState({
     name: '',
     capacity: '',
     unit: 'kg',
     quantity: '',
+    current_quantity: '',
     category: 'glassware',
     condition: '',
     date: ''
@@ -28,13 +31,19 @@ export default function AddItemDialog({ onDone }) {
 
   const handleClose = () => {
     setOpen(false);
+    setLoading(false); // Reset loading state
   };
 
   const handleDone = async () => {
-    // Perform any necessary actions here
-    await uploadTE(item); // Call the uploadTE function to add the item to Firestore
-    onDone(); // Call the onDone function to trigger the refresh
-    handleClose();
+    setLoading(true); // Set loading state to true
+    try {
+      await uploadTE(item); // Call the uploadTE function to add the item to Firestore
+      onDone(); // Call the onDone function to trigger the refresh
+      handleClose();
+    } catch (error) {
+      console.error('Error uploading item:', error);
+      setLoading(false); // Reset loading state in case of error
+    }
   };
 
   return (
@@ -55,83 +64,90 @@ export default function AddItemDialog({ onDone }) {
       >
         <DialogTitle>Add Tool/Equipment</DialogTitle>
         <DialogContent>
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Name"
-                variant="outlined"
-                value={item.name}
-                onChange={(e) => setItem(prevItem => ({ ...prevItem, name: e.target.value }))}
-              />
+          {loading ? (
+            <Grid container justifyContent="center" alignItems="center" style={{ height: '100px' }}>
+              <CircularProgress />
+              <p>Please wait...</p>
             </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Quantity"
-                variant="outlined"
-                value={item.quantity}
-                onChange={(e) => setItem(prevItem => ({ ...prevItem, quantity: e.target.value }))}
-                helperText="Please enter the capacity of the item"
-              />
-            </Grid>
+          ) : (
+            <Grid container spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="Name"
+                  variant="outlined"
+                  value={item.name}
+                  onChange={(e) => setItem(prevItem => ({ ...prevItem, name: e.target.value }))}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="Quantity"
+                  variant="outlined"
+                  value={item.quantity}
+                  onChange={(e) => setItem(prevItem => ({ ...prevItem, quantity: e.target.value }))}
+                  helperText="Please enter the capacity of the item"
+                />
+              </Grid>
 
-            <Grid item xs={3}>
-              <TextField
-                fullWidth
-                label="Capacity"
-                variant="outlined"
-                value={item.capacity}
-                onChange={(e) => setItem(prevItem => ({ ...prevItem, capacity: e.target.value }))}
-                helperText="Please enter the capacity of the item"
-              />
-            </Grid>
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  label="Capacity"
+                  variant="outlined"
+                  value={item.capacity}
+                  onChange={(e) => setItem(prevItem => ({ ...prevItem, capacity: e.target.value }))}
+                  helperText="Please enter the capacity of the item"
+                />
+              </Grid>
 
-            <Grid item xs={3}>
-              <TextField
-                fullWidth
-                label="Unit"
-                variant="outlined"
-                select
-                value={item.unit}
-                onChange={(e) => setItem(prevItem => ({ ...prevItem, unit: e.target.value }))}
-                SelectProps={{ native: true }}
-                helperText="Please select the unit of measurement"
-              >
-                <option value="kg">kg</option>
-                <option value="g">g</option>
-                <option value="L">L</option>
-                <option value="mL">mL</option>
-              </TextField>
+              <Grid item xs={3}>
+                <TextField
+                  fullWidth
+                  label="Unit"
+                  variant="outlined"
+                  select
+                  value={item.unit}
+                  onChange={(e) => setItem(prevItem => ({ ...prevItem, unit: e.target.value }))}
+                  SelectProps={{ native: true }}
+                  helperText="Please select the unit of measurement"
+                >
+                  <option value="kg">kg</option>
+                  <option value="g">g</option>
+                  <option value="L">L</option>
+                  <option value="mL">mL</option>
+                </TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="Category"
+                  variant="outlined"
+                  select
+                  value={item.category}
+                  onChange={(e) => setItem(prevItem => ({ ...prevItem, category: e.target.value }))}
+                  SelectProps={{ native: true }}
+                  helperText="Please select the category"
+                >
+                  <option value="all">All</option>
+                  <option value="glassware">Glassware</option>
+                  <option value="plasticware">Plasticware</option>
+                  <option value="metalware">Metalware</option>
+                  <option value="heating">Heating</option>
+                  <option value="measuring">Measuring</option>
+                  <option value="container">Container</option>
+                  <option value="separator">Separation Equipment</option>
+                  <option value="mixing">Mixing & Stirring</option>
+                </TextField>
+              </Grid>
+              {/* Add other fields as necessary */}
             </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                label="Category"
-                variant="outlined"
-                select
-                value={item.category}
-                onChange={(e) => setItem(prevItem => ({ ...prevItem, category: e.target.value }))}
-                SelectProps={{ native: true }}
-                helperText="Please select the category"
-              >
-                <option value="all">All</option>
-                <option value="glassware">Glassware</option>
-                <option value="plasticware">Plasticware</option>
-                <option value="metalware">Metalware</option>
-                <option value="heating">Heating</option>
-                <option value="measuring">Measuring</option>
-                <option value="container">Container</option>
-                <option value="separator">Separation Equipment</option>
-                <option value="mixing">Mixing & Stirring</option>
-              </TextField>
-            </Grid>
-            {/* Add other fields as necessary */}
-          </Grid>
+          )}
         </DialogContent>
         <DialogActions>
-          <Button aria-hidden="true" onClick={handleClose}>Cancel</Button>
-          <Button aria-hidden="true" onClick={handleDone}>Done</Button>
+          <Button aria-hidden="true" onClick={handleClose} disabled={loading}>Cancel</Button>
+          <Button aria-hidden="true" onClick={handleDone} disabled={loading}>Done</Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
