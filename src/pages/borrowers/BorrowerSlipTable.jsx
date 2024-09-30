0a9069221d58e03705bee47f8991059d7fa3b152
caloy <21-1-02391@vsu.edu.ph>
2024-09-30 21:04:24 +0800

    some adjustment in borrowers page

--- src/pages/borrowers/BorrowerSlipTable.jsx
+++ src/pages/borrowers/BorrowerSlipTable.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { List, Box } from '@mui/material';
+import { List, Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 // project import
 import BorrowerSlip from './BorrowerSlip';
 
-//database imports
+// database imports
 import { fetchAllBorrowers } from 'pages/TE_Backend';
 
 const avatarSX = {
@@ -18,7 +18,7 @@ const actionSX = {
   right: 0
 };
 
-export default function BorrowerSlipTable({ status}) {
+export default function BorrowerSlipTable({ status }) {
   const [borrowers, setBorrowers] = useState([]);
 
   useEffect(() => {
@@ -32,24 +32,34 @@ export default function BorrowerSlipTable({ status}) {
   // Ensure borrowers is defined and is an array before filtering
   const approvedBorrowers = Array.isArray(borrowers) ? borrowers.filter(borrower => borrower.isApproved === status) : [];
 
+  // Define the height based on whether there are approved borrowers or not and if the status is 'admin approved'
+  const boxHeight = status === 'admin approved' && approvedBorrowers.length === 0 ? 'auto' : '655px';
+  const overflowY = status === 'admin approved' && approvedBorrowers.length === 0 ? 'visible' : 'auto';
+
   return (
-    <Box sx={{ height: '655px', overflowY: 'auto' }}> {/* Set a fixed height and make it scrollable */}
-      <List
-        component="nav"
-        sx={{
-          px: 0,
-          py: 0,
-          '& .MuiListItemButton-root': {
-            py: 1.5,
-            '& .MuiAvatar-root': avatarSX,
-            '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-          }
-        }}
-      >
-        {approvedBorrowers.map(borrower => (
-          <BorrowerSlip borrower={borrower} status={status} key={borrower.id} />
-        ))}
-      </List>
+    <Box sx={{ height: boxHeight, overflowY: overflowY }}>
+      {approvedBorrowers.length === 0 ? (
+        <Typography variant="h6" align="center" sx={{ mt: 2 }}>
+          No borrower slips found.
+        </Typography>
+      ) : (
+        <List
+          component="nav"
+          sx={{
+            px: 0,
+            py: 0,
+            '& .MuiListItemButton-root': {
+              py: 1.5,
+              '& .MuiAvatar-root': avatarSX,
+              '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
+            }
+          }}
+        >
+          {approvedBorrowers.map(borrower => (
+            <BorrowerSlip borrower={borrower} status={status} key={borrower.id} />
+          ))}
+        </List>
+      )}
     </Box>
   );
 }
